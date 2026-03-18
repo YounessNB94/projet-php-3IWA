ff005c15f3768dd80c37160f6cb6e3ce5bb4ae96
YounessNB94 <younessnb@hotmail.fr>
2026-03-18 16:16:27 +0100

    add auth routes for login, register, and logout

---
+++ routes/auth.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+$router->get('/login', [AuthController::class, 'showLogin']);
+$router->post('/login', [AuthController::class, 'login']);
+
+$router->get('/register', [AuthController::class, 'showRegister']);
+$router->post('/register', [AuthController::class, 'register']);
+
+$router->get('/logout', [AuthController::class, 'logout']);
